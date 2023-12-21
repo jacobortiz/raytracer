9dc1971dbfe893ae2aa0337cab1569cfc81c2543
jacobortiz <jacobortiz90@gmail.com>
2023-12-21 15:20:32 -0800

    image output

--- main.cpp
+++ main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
 
 int main() {
-    std::cout << "hello world!";
-    return 0;
+    
+    // image
+    
+    int image_width = 256;
+    int image_height = 256;
+
+    // render
+
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+    for (int j = 0; j < image_height; ++j) {
+        std::clog << "\rScanlines remaining: " << image_height - j << ' ' << std::flush;
+        for (int i = 0; i < image_width; ++i) {
+            auto r = double(i) / (image_width - 1);
+            auto g = double(j) / (image_height - 1);
+            auto b = 0.25;
+
+            int ir = static_cast<int>(255.999 * r);
+            int ig = static_cast<int>(255.999 * g);
+            int ib = static_cast<int>(255.999 * b);
+
+            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+        }
+    }
+
+    std::clog << "\rDone.\t\t\t\n";
 }
